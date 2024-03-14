ae0189f1c11c5005147582fe7166cb159d61f74f
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2024-03-14 13:38:48 +0400

    end task

--- src/App.js
+++ src/App.js
@@ -1,59 +1,43 @@
-import { useState } from "react";
-
-function useGeolocation() {}
+import {useState} from "react";
+import {useGeolocation} from "./useGeolocation";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
+    const {
+        isLoading,
+        position: { lat, lng },
+        error,
+        getPosition
+    } = useGeolocation();
 
-  const { lat, lng } = position;
+    const [countClicks, setCountClicks] = useState(0);
 
-  function getPosition() {
-    setCountClicks((count) => count + 1);
+    function handleClick() {
+        setCountClicks((count) => count + 1);
+        getPosition();
+    }
 
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
+    return (
+        <div>
+            <button onClick={handleClick} disabled={isLoading}>
+                Get my position
+            </button>
 
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          setPosition({
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude
-          });
-          setIsLoading(false);
-        },
-        (error) => {
-          setError(error.message);
-          setIsLoading(false);
-        }
+            {isLoading && <p>Loading position...</p>}
+            {error && <p>{error}</p>}
+            {!isLoading && !error && lat && lng && (
+                <p>
+                    Your GPS position:{" "}
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+                    >
+                        {lat}, {lng}
+                    </a>
+                </p>
+            )}
+
+            <p>You requested position {countClicks} times</p>
+        </div>
     );
-  }
-
-  return (
-      <div>
-        <button onClick={getPosition} disabled={isLoading}>
-          Get my position
-        </button>
-
-        {isLoading && <p>Loading position...</p>}
-        {error && <p>{error}</p>}
-        {!isLoading && !error && lat && lng && (
-            <p>
-              Your GPS position:{" "}
-              <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
-              >
-                {lat}, {lng}
-              </a>
-            </p>
-        )}
-
-        <p>You requested position {countClicks} times</p>
-      </div>
-  );
 }
